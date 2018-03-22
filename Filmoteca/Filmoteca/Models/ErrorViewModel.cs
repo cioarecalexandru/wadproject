365363099e187ea0ab8d48de2a6d63aca81cd206
cioarecalexandru <cioarec.alexandru96@yahoo.com>
2018-03-22 22:46:03 +0200

    Client firts commit

---
+++ Filmoteca/Filmoteca/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Filmoteca.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
